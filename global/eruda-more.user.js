a776058907bfe108dbca16ab376db9bc82d728f6
Hezekiah Michael <spiritomb@protonmail.com>
2022-11-04 15:31:49 -0400

    add eslint comment and lint

--- global/eruda-more.user.js
+++ global/eruda-more.user.js
@@ -10,13 +10,15 @@
 // @require     https://cdn.jsdelivr.net/npm/eruda-code@2.0.0
 // @require     https://cdn.jsdelivr.net/npm/eruda-dom@2.0.0
 // @connect     chrome-extension://*
-// @description Eruda mobile devtools as a userscript, for troubleshooting on mobile, targeting Vivaldi via Adguard for Android.
+// @description Eruda mobile devtools as a userscript, for troubleshooting on mobile, targeting Vivaldi via Adguard for Android. It doesn't work yet.
 // ==/UserScript==
-
+/* globals eruda, erudaCode, erudaDom */
 (function () {
     // if (!/eruda=true/.test(window.location) && localStorage.getItem('active-eruda') != 'true') return;
     eruda.init({
-        tool: ['console', 'elements', 'info', 'resources', 'sources','snippets'],
+        tool: [
+            'console', 'elements', 'info', 'resources', 'sources', 'snippets'
+        ],
         autoScale: true,
         defaults: {
             displaySize: 40,
@@ -24,35 +26,36 @@
             theme: 'Material Palenight'
         }
     });
-	let console = eruda.get('console');
-	let resources = eruda.get('resources');
-	let elements = eruda.get('elements');
-	let sources = eruda.get('sources');
-	let snippets = eruda.get('snippets');
-	console.config.set('asyncRender', true);
-	console.config.set('jsExecution', true);
-	console.config.set('catchGlobalErr', true);
-	console.config.set('overrideConsole', true);
-	console.config.set('displayExtraInfo', true);
-	console.config.set('displayUnenumerable', true);
-	console.config.set('displayGetterVal', true);
-	console.config.set('lazyEvaluation', true);
-	console.config.set('displayIfErr', true);
-	resources.config.set('hideErudaSetting', false);
-	elements.config.set('overrideEventTarget', true);
-	sources.config.set('indentSize','4');
-	sources.config.set('formatCode', false);
-	sources.config.set('showLineNum', true);
-	snippets.add('hello', function () {
+    let console = eruda.get('console');
+    let resources = eruda.get('resources');
+    let elements = eruda.get('elements');
+    let sources = eruda.get('sources');
+    let snippets = eruda.get('snippets');
+    console.config.set('asyncRender', true);
+    console.config.set('jsExecution', true);
+    console.config.set('catchGlobalErr', true);
+    console.config.set('overrideConsole', true);
+    console.config.set('displayExtraInfo', true);
+    console.config.set('displayUnenumerable', true);
+    console.config.set('displayGetterVal', true);
+    console.config.set('lazyEvaluation', true);
+    console.config.set('displayIfErr', true);
+    resources.config.set('hideErudaSetting', false);
+    elements.config.set('overrideEventTarget', true);
+    sources.config.set('indentSize', '4');
+    sources.config.set('formatCode', false);
+    sources.config.set('showLineNum', true);
+    snippets.add('hello', function () {
     	console.log('Hello World!');
-	}, 'Display hello on console');
+    }, 'Display hello on console');
 
     eruda.position({x: 20, y: 20});
     eruda.show();
-	eruda.add(erudaCode);
-	eruda.add(erudaDom);
-	console.log('eruda is a console for %s.', 'mobile browsers');
-	console.table([{test: 1}, {test: 2}, {test2: 3}], 'test');
-	console.error(new Error('eruda'));
+    eruda.add(erudaCode);
+    eruda.add(erudaDom);
+    console.log('eruda is a console for %s.', 'mobile browsers');
+    console.table([
+        {test: 1}, {test: 2}, {test2: 3}
+    ], 'test');
+    console.error(new Error('eruda'));
 })();
-
