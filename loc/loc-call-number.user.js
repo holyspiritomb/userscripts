7baf5476d09231d2d0de3f168ea9c20a7b3f9b3b
Hezekiah Michael <pianycist@gmail.com>
2022-12-17 18:19:03 -0500

    add scrip5

---
+++ loc/loc-call-number.user.js
@@ -0,0 +1,49 @@
+// ==UserScript==
+// @name        LOC call number copy
+// @namespace   https://github.com/holyspiritomb
+// @author      holyspiritomb
+// @version     0.0.1
+// @description Copy the coad
+// @homepageURL https://github.com/holyspiritomb/userscripts
+// @license     MIT
+// @match       https://id.loc.gov/resources/works/*
+// @run-at      document-idle
+// @grant       GM_setClipboard
+// @grant       unsafeWindow
+// @require     https://code.jquery.com/jquery-latest.min.js
+// ==/UserScript==
+
+var lccNumber;
+var workTitle;
+var authorName;
+
+workTitle = $("dt[title='bf:Title'] + dd")[0].innerText.replace(" : ", ": ");
+
+//if ($("div.bf-display-contribution a[href*='/authorities/names/']").length == 1){
+//    authorName = $("div.bf-display-contribution a[href*='/authorities/names/']").text();
+//    alert(authorName);
+//}
+
+$("div.bf-display-classification > dd").each(
+    function() {
+        let catalogType = this.childNodes[0].innerText;
+        if (catalogType == "LCC: ") {
+            lccNumber = this.childNodes[1].wholeText;
+            $("h1").after(`<div id="callnumber">${lccNumber} <span></span></div>`);
+            $("div#callnumber > span").append('\u{1F4CB}');
+        }
+});
+
+if (lccNumber != null) {
+    $("div#callnumber > span").on('click', function(){
+        GM_setClipboard(lccNumber);
+        this.innerHTML = "";
+        this.append('\u2713');
+    });
+}
+
+$("h1").after(`<div id="workTitle">${workTitle} </div>`);
+$("div#workTitle").on('click', function(){
+    GM_setClipboard(workTitle);
+    this.append('\u2713');
+});
