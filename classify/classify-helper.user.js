0a69ca56bdd2f3cd24fc160a4ab2d6a9754cf237
Hezekiah Michael <pianycist@gmail.com>
2023-03-17 12:36:57 -0400

    add info to classify helper

---
+++ classify/classify-helper.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name        Classify helper
+// @namespace   https://github.com/holyspiritomb
+// @match       *://classify.oclc.org/classify2/ClassifyDemo*
+// @grant       GM_setClipboard
+// @version     1.0
+// @author      holyspiritomb
+// @require     https://code.jquery.com/jquery-latest.min.js
+// @description Make using the web demo easier for copying data.
+// ==/UserScript==
+
+
+let owilink = $('a[href^="http://classify.oclc.org/classify2/ClassifyDemo?owi="]');
+
