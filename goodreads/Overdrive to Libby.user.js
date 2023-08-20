1c37bfd704b325e77f753425eae253f9ea473f06
Hezekiah Michael <pianycist@gmail.com>
2023-08-19 20:00:38 -0400

    set timeout by location and add icon

--- goodreads/Overdrive to Libby.user.js
+++ goodreads/Overdrive to Libby.user.js
@@ -2,10 +2,12 @@
 // @name        Overdrive URL to Libby
 // @namespace   https://github.com/holyspiritomb
 // @author      spiritomb
-// @version     0.0.2
+// @version     0.0.3
 // @description Changes Overdrive library links to Libby links.
 // @match       https://www.goodreads.com/book/show/*
 // @match       https://www.goodreads.com/review/list/*
+// @match       https://www.goodreads.com/list/*
+// @match       https://www.goodreads.com/search*
 // @run-at      document-end
 // @grant       none
 // @homepageURL https://github.com/holyspiritomb/userscripts
@@ -14,11 +16,22 @@
 // @require     https://code.jquery.com/jquery-latest.min.js
 // ==/UserScript==
 
-function odToLibby(a) {
-    let url = a.href;
+function odToLibby(elem) {
+    let url = elem.href;
     let libbyUrl = url.replace("http:", "https:").replace("://", "://libbyapp.com/search/").replace(".overdrive.com/search/title?query=", "/search/query-").replace("&creator=", "%20");
     libbyUrl = libbyUrl + "/page-1";
     return libbyUrl;
+}
+
+var timeout;
+if (location.href.startsWith("https://www.goodreads.com/list")) {
+    timeout = 15000;
+} else if (location.href.startsWith("https://www.goodreads.com/review")) {
+    timeout = 10000;
+} else if (location.href.startsWith("https://www.goodreads.com/search")) {
+    timeout = 10000;
+} else {
+    timeout = 5000;
 }
 
 setTimeout( function () {
@@ -26,4 +39,8 @@ setTimeout( function () {
         this.href = odToLibby(this);
         console.log(this.href);
     });
-}, 5000);
+    $('img.AGaudio').each(function() {
+        this.before('\uD83C\uDFA7');
+        this.style.display = "none";
+    });
+}, timeout);
