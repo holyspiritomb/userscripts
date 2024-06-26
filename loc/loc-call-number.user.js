bf6d6bbce48eddd8de23ef35c13c75ab0e8291e0
Hezekiah Michael <spiritomb@protonmail.com>
2024-06-26 13:37:40 -0400

    update

--- loc/loc-call-number.user.js
+++ loc/loc-call-number.user.js
@@ -2,20 +2,39 @@
 // @name        LOC call number copy
 // @namespace   https://github.com/holyspiritomb
 // @author      holyspiritomb
-// @version     0.0.6
+// @version     0.0.7
 // @description Copy the call number 
 // @homepageURL https://github.com/holyspiritomb/userscripts
 // @license     MIT
 // @match       https://id.loc.gov/resources/works/*
 // @run-at      document-idle
-// @grant       GM_setClipboard
 // @grant       unsafeWindow
+// @grant       GM_setClipboard
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @grant       GM_listValues
 // @require     https://code.jquery.com/jquery-latest.min.js
 // ==/UserScript==
 
 var lccNumber;
 var workTitle;
 var authorName;
+
+let valueArrayList = GM_listValues();
+
+if (!valueArrayList) {
+    GM_setValue("debug", false);
+}
+
+var debug = GM_getValue("debug");
+
+function logger(logMsg){
+    if (debug){
+        console.log(logMsg);
+    } else {
+        return
+    }
+}
 
 workTitle = $("dt[title='bf:Title'] + dd")[0].innerText.replace(" : ", ": ");
 
@@ -24,27 +43,38 @@ workTitle = $("dt[title='bf:Title'] + dd")[0].innerText.replace(" : ", ": ");
 //    alert(authorName);
 //}
 
+
 $("div.bf-display-classification > dd").each(
     function() {
-        let catalogType = this.childNodes[0].innerText;
-        if (catalogType == "LCC: ") {
-            lccNumber = this.innerText.replace("LCC: ","").replace(" (Assigner: dlc)","");
-            $("h1").after(`<div id="callnumber">${lccNumber} <span></span></div>`);
-            $("div#callnumber > span").append('\u{1F4CB}');
+        let wholeThing = this.childNodes[0].wholeText;
+        let catalogType = this.childNodes[0].wholeText.split(":")[0];
+        let year = wholeThing.slice(-5);
+        logger(wholeThing)
+        logger(catalogType)
+        logger(year)
+        if (catalogType == "LCC") { // perhaps: if (wholeThing.startsWith("LCC"))
+            logger(this)
+            let callNumber = wholeThing.replace("LCC: ", "").replace("  ", "");
+            logger(callNumber);
+            lccNumber = callNumber;
+            logger(lccNumber);
+            // lccNumber = this.innerText.replace("LCC: ", "").replace(" (Assigner: dlc)", "");
+            $("h1").after(`<div id="callnumber">${lccNumber} <span> </span></div>`);
+            $("div#callnumber > span").append('\u{1F4CB}'); //clipboard emoji
         }
 });
 
 if (lccNumber != null) {
-    lccNumber.replace("  ", "");
     $("div#callnumber > span").on('click', function(){
         GM_setClipboard(lccNumber);
         this.innerHTML = "";
-        this.append('\u2713');
+        // this.append('\u2713');
+        this.append('\u{1f44d}'); //thumbs up emoji
     });
 }
 
-$("h1").after(`<div id="workTitle">${workTitle} </div>`);
-$("div#workTitle").on('click', function(){
-    GM_setClipboard(workTitle);
-    this.append('\u2713');
-});
+$("h1").after(`<div id="workTitle">${workTitle}</div>`);
+// $("div#workTitle").on('click', function(){
+//     GM_setClipboard(workTitle);
+//     this.append('\u2713');
+// });
